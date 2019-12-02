3e450367d30ac1168da0beae644eb1c88f835bc5
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-12-02 15:18:30 +0600

    Create terminal.bash

---
+++ terminal.bash
@@ -0,0 +1,5 @@
+The Best Linux Blog In the Unixverse
+Useless yet funny stuff. Embrace your inner child. Bring back Basic programming days with bash ;) 
+
+lidan@suidan$ P=(' ' █ ░ ▒ ▓)
+lidan@suidan$ while :;do printf "\e[$[RANDOM%LINES+1];$[RANDOM%COLUMNS+1]f${P[$RANDOM%5]}";done
