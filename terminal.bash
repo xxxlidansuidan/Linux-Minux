4cc6f722422dde3aa050a30148eb9ba29940e527
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-12-02 17:39:48 +0600

    Update terminal.bash

--- terminal.bash
+++ terminal.bash
@@ -1,5 +1,24 @@
 The Best Linux Blog In the Unixverse
 Useless yet funny stuff. Embrace your inner child. Bring back Basic programming days with bash ;) 
+* FYI, might eat up your CPU. So do not run in prod :P
 
-lidan@suidan$ P=(' ' █ ░ ▒ ▓)
-lidan@suidan$ while :;do printf "\e[$[RANDOM%LINES+1];$[RANDOM%COLUMNS+1]f${P[$RANDOM%5]}";done
+$ P=(' ' █ ░ ▒ ▓)
+$ while :;do printf "\e[$[RANDOM%LINES+1];$[RANDOM%COLUMNS+1]f${P[$RANDOM%5]}";done
+
+Or,# added nice for slow....
+
+$ P=(' ' █ ░ ▒ ▓)
+$ while :;do nice printf "\e[$[RANDOM%LINES+1];$[RANDOM%COLUMNS+1]f${P[$RANDOM%5]}";done
+
+***This is the same, but with color, but seems to slow it down a bit...
+
+P=(' ' █ ░ ▒ ▓)
+while :;do printf "\e[9$(( ( RANDOM % 7 )  + 1 ))m\e[$[RANDOM%LINES+1];$[RANDOM%COLUMNS+1]f${P[$RANDOM%5]}";done
+
+it works with zsh: 
+
+#!/bin/zsh
+P=(' ' █ ░ ▒ ▓)
+while :;do
+printf "\e[9$(( ( RANDOM % 7 )  + 1 ))m\e[$[RANDOM%$LINES+1];$[RANDOM%$COLUMNS+1]f${P[$RANDOM%5]}" 
+done
