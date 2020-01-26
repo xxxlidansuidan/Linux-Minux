6cbcd95ed129d6e4a6ceb61e1d914c06edc8d96e
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2020-01-26 10:28:23 +0600

    Update bash_aliases.sh

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,3 +1,7 @@
+#color code in bash
+30 Black, 31 Red, 32 Green, 33 Yellow, 34 Blue, 35 Magenta, 36 Cyan, 37 Light gray, 90 Dark gray, 91 Light red, 92 Light green, 93 Light yellow, 94 Light blue, 95 Light magenta, 96 Light cyan, 97 White;
+
+#working on bash========================
 alias bold=' echo -e "Normal \e[1mBold"'
 alias underline='echo -e "Normal \e[4mUnderlined"'
 alias blink='echo -e "Normal \e[5mBlink"'
